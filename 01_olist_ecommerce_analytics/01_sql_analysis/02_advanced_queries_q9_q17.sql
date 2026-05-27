a45ab46b8a9358b9542a8b2005e7ed2910d71731
Petermchikho <petercharlesmchikho1@gmail.com>
2026-05-27 08:17:12 +0200

    Add SQL queries to analyze delivery delays for sellers with at least 20 orders

--- 01_olist_ecommerce_analytics/01_sql_analysis/02_advanced_queries_q9_q17.sql
+++ 01_olist_ecommerce_analytics/01_sql_analysis/02_advanced_queries_q9_q17.sql
@@ -68,3 +68,64 @@ from total_month_gmv;
 
 -- 11. For each seller with at least 20 delivered orders, what is the average, median, 90th and 95th 
 --percentile delivery delay in days relative to the estimated delivery date?
+
+WITH order_items_sellers AS (
+
+SELECT DISTINCT(order_id) as order_id,seller_id FROM olist_order_items
+
+),twenty_above_orders_sellers as(
+select 
+	oo.order_delivered_customer_date,
+	oo.order_estimated_delivery_date,
+	oo.order_id,ois.seller_id,
+	round((extract(epoch from oo.order_estimated_delivery_date) 
+	- extract(epoch from oo.order_delivered_customer_date)) 
+	/ (60*60*24)) as delivery_diff_days,
+	count(oo.order_id) over(partition by ois.seller_id) as total_orders,
+	CASE 
+	        WHEN round((extract(epoch from oo.order_estimated_delivery_date) 
+	- extract(epoch from oo.order_delivered_customer_date)) 
+	/ (60*60*24)) < 0 THEN ABS(round((extract(epoch from oo.order_estimated_delivery_date) 
+	- extract(epoch from oo.order_delivered_customer_date)) 
+	/ (60*60*24)))
+	        ELSE 0
+	END AS delayed_days
+from olist_orders oo left join order_items_sellers ois on oo.order_id = ois.order_id
+where oo.order_status = 'delivered'
+),with_averages as(
+SELECT 
+seller_id,
+total_orders,
+delivery_diff_days,
+order_id,
+delayed_days,
+avg(delayed_days) over() as average_delay_days,
+avg(delayed_days) over(partition by seller_id) as average_delay_days_for_seller
+FROM twenty_above_orders_sellers
+where total_orders>=20
+), with_percentiles as(
+select 
+PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY delayed_days) AS p25,
+PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY delayed_days) AS p50_median,
+PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY delayed_days) AS p75,
+PERCENTILE_CONT(0.90) WITHIN GROUP (ORDER BY delayed_days) AS p90
+from with_averages
+WHERE delayed_days > 0
+)
+SELECT
+    wa.seller_id,
+    wa.total_orders,
+    wa.delivery_diff_days,
+    wa.order_id,
+    wa.delayed_days,
+    wa.average_delay_days,
+    wa.average_delay_days_for_seller,
+    wp.p25,
+    wp.p50_median,
+    wp.p75,
+    wp.p90
+FROM with_averages wa
+CROSS JOIN with_percentiles wp
+where wa.delayed_days > 0
+;
+
